5d021d5ae06e6f50498bf904299efcf7362cde22
Matvey Eikholts <matze85eikholts@gmail.com>
2025-04-05 09:05:23 +0600

    basic class entry point

---
+++ CSharpLearnProject/Person.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace CSharpLearnProject
+{
+    public class Person
+    {
+        // Свойства класса
+        public string Name { get; set; }
+        public int Age { get; set; }
+       
+        // Конструктор по умолчанию
+        public Person()
+        {
+            Name = "Valera";
+            Age = 0;
+        }
+
+        // Параметризованный конструктор
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        // Метод для вывода информации о человеке
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+}
---
+++ CSharpLearnProject/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+//
+//Console.WriteLine("Hello, World!");
+
+using System;
+using CSharpLearnProject;
+
+public class MainClass {
+
+    static int Sum(int a, int b) {
+        int sum = 0;
+        return sum = a + b;
+    }
+    public static void Main()
+    {
+        int a = 2, b = 3;
+        int sum = Sum(a, b);    
+        Console.WriteLine("Hello, my dear friend!!!");
+        Console.WriteLine(sum);
+        // Создание объекта с использованием конструктора по умолчанию
+
+        Person person1 = new Person();
+        Console.WriteLine("Using default constructor:");
+        person1.DisplayInfo();
+
+        // Создание объекта с использованием параметризованного конструктора
+        Person person2 = new Person("Alice", 30);
+        Console.WriteLine("\nUsing parameterized constructor:");
+        person2.DisplayInfo();
+    }
+}
