74959d1fdb55d4c5052aa4a538323049f058a29a
Matvey Eikholts <matze85eikholts@gmail.com>
2025-04-08 20:59:10 +0600

    class from interface

---
+++ CSharpLearnProject/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearnProject
+{
+    public class Student : IPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Grade { get; set; } // Дополнительное свойство для студента
+
+        public Student(string firstName, string lastName, int grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+        public void Introduce()
+        {
+            Console.WriteLine($"Привет! Я студент {FirstName} {LastName}. Учусь в {Grade} классе.");
+        }
+
+
+    }
+}
